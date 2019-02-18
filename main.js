a8400df0a8072daba987c154e4f591b9a7e411bb
omar-nav <omar.navarro@g.austincc.edu>
2019-02-18 16:14:44 -0600

    TODO create form for reviews

--- main.js
+++ main.js
@@ -22,63 +22,62 @@ Vue.component("product", {
     }
   },
   template: `
-  <div class="product">
-  <div class="product-image">
-    <img :src="image" />
-  </div>
+   <div class="product">
+        
+      <div class="product-image">
+        <img :src="image" />
+      </div>
 
-  <div class="product-info">
-    <h1>{{ title }}</h1>
-    <p v-if="inStock">In Stock</p>
-    <p v-else>Out of Stock</p>
-    <p>{{ sale }}</p>
-    <p>Shipping: {{ shipping }}</p>
+      <div class="product-info">
+          <h1>{{ product }}</h1>
+          <p v-if="inStock">In Stock</p>
+          <p v-else>Out of Stock</p>
+          <p>Shipping: {{ shipping }}</p>
 
-    <product-details :details="details"></product-details>
+          <ul>
+            <li v-for="detail in details">{{ detail }}</li>
+          </ul>
 
-    <div
-      class="color-box"
-      v-for="(variant, index) in variants"
-      :key="variant.variantId"
-      :style="{ backgroundColor: variant.variantColor }"
-      @mouseover="updateProduct(index)"
-    ></div>
+          <div class="color-box"
+               v-for="(variant, index) in variants" 
+               :key="variant.variantId"
+               :style="{ backgroundColor: variant.variantColor }"
+               @mouseover="updateProduct(index)"
+               >
+          </div> 
 
-    <button
-      v-on:click="addToCart"
-      :disabled="!inStock"
-      :class="{ disabledButton: !inStock }"
-    >
-      Add to cart
-    </button>
-    <button @click="removeFromCart">
-      Remove from cart
-    </button>
+          <button v-on:click="addToCart" 
+            :disabled="!inStock"
+            :class="{ disabledButton: !inStock }"
+            >
+          Add to cart
+          </button>
+          <button @click="removeFromCart">
+          Remove from cart
+        </button>
 
+       </div>  
     
-  </div>
-</div>
-`,
+    </div>
+   `,
   data() {
     return {
+      product: "Socks",
       brand: "Vue Mastery",
-      product: "socks",
       selectedVariant: 0,
-      details: ["100% real", "80% cotton", "20% polyester", "Gender-neutral"],
+      details: ["80% cotton", "20% polyester", "Gender-neutral"],
       variants: [
         {
           variantId: 2234,
           variantColor: "green",
-          variantImage: "./greenSocks.jpeg",
-          variantQuantity: 10,
-          onSale: true
+          variantImage: "greenSocks.jpeg",
+          variantQuantity: 10
         },
         {
           variantId: 2235,
           variantColor: "blue",
-          variantImage: "./blueSocks.jpeg",
-          variantQuantity: 0,
-          onSale: false
+          variantImage: "blueSocks.jpeg",
+          variantQuantity: 0
         }
       ]
     }
@@ -89,7 +88,6 @@ Vue.component("product", {
     },
     updateProduct(index) {
       this.selectedVariant = index
-      console.log(index)
     },
     removeFromCart() {
       this.$emit(
@@ -100,7 +98,7 @@ Vue.component("product", {
   },
   computed: {
     title() {
-      return this.brand + "" + this.product
+      return this.brand + " " + this.product
     },
     image() {
       return this.variants[this.selectedVariant].variantImage
@@ -108,18 +106,11 @@ Vue.component("product", {
     inStock() {
       return this.variants[this.selectedVariant].variantQuantity
     },
-    sale() {
-      if (this.onSale) {
-        return this.brand + " " + this.product + " are on sale!"
-      } else {
-        return this.brand + " " + this.product + " are not on sale."
-      }
-    },
     shipping() {
       if (this.premium) {
         return "Free"
       }
-      return "$3 MXN"
+      return 2.99
     }
   }
 })
@@ -127,7 +118,7 @@ Vue.component("product", {
 var app = new Vue({
   el: "#app",
   data: {
-    premium: false,
+    premium: true,
     cart: []
   },
   methods: {
